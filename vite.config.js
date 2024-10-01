11a31f92f7a3ed752b6c671406cda1aecc18f05f
yasmine <guerricheyasmine@gmail.com>
2024-10-01 14:09:05 +0200

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-yasmine/', // Ajoute ton chemin ici
+  base: '/portfolio-yasmine/',
 });
 
