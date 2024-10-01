32418d2df8afc0384b20d4b113c725bb8abaf427
yasmine <guerricheyasmine@gmail.com>
2024-10-01 13:58:18 +0200

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig(
-  {
+export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-yasmine/', // Ajoute ton chemin ici
 });
+
